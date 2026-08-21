907a4efadfe228ce576c3f404dc8b16f6516c637
Cristian Orjuela <cristiand-orjuelam@unilibre.edu.co>
2026-08-21 20:37:59 +0000

    Clase 21/08/2026 utilización de props en react y props con colores la primera llave es css y la segunda del props

--- react-unilibre/src/App.jsx
+++ react-unilibre/src/App.jsx
@@ -1,9 +1,23 @@
 import './App.css'
 
+//vamos a crear un componente
+function Person(props){
+  return(
+    <div className="person" style={{backgroundColor:props.color}}>
+      <h3>Nombre: {props.nombre}</h3>
+      <p>Edad: {props.edad}</p>
+      <p>Salario: {props.salario}</p>
+    </div>
+  )
+}
 export default function App() {
   return (
     <div className="App">
       <h1>Hola Mundo</h1>
+      {/*Vamos a añadir un componente*/}
+      <Person nombre="Cristian" edad="22" salario="50.000.000" color="#abc440"/>
+      <Person nombre="Santiago" edad="23" salario="23.000.000" color="black"/>
+      <Person nombre="Brandon" edad="23" salario="25.000.000" color="orange"/>
     </div>
     );
 }
