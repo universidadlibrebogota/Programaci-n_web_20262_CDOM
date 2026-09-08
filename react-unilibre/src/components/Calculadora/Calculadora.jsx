b6e7ac5cacafe7a353e891e49c92005e7d02a698
Cristian Orjuela <cristiand-orjuelam@unilibre.edu.co>
2026-09-08 20:30:12 +0000

    8 de sept 2026 calculadora por el momento no calcula logicamente y uso de styles nuevos dentro de div

---
+++ react-unilibre/src/components/Calculadora/Calculadora.jsx
@@ -0,0 +1,82 @@
+import {useState} from 'react';
+export function Calculadora(){
+    // Estados o hooks para los numeros, signos y resultado
+    const [num1, setNum1]= useState('');
+    const [num2, setNum2]= useState('');
+    const [rta, setRta]= useState(null);
+    const [signo, setSigno]= useState('');
+    // Funcion que realiza los calculos
+    const calcular=(e)=>{
+        e.preventDefault();
+        const n1= parseFloat(num1);
+        const n2= parseFloat(num2);
+        //Validamos si los numeros vienen vacios
+        if (isNaN(n1) || !num1 || isNaN(n2) || !num2){
+            setRta('Debo digitar los dos numeros');
+            return;
+        }
+        let result;
+        switch(signo){
+            case '+': result = n1 + n2;
+                    break;
+            case '-': result = n1 - n2;
+                break;
+            case '*': result = n1 * n2;
+                    break;
+            case '/': if (n2==0){
+                        result ='NO se puede dividr por 0';
+                        }else{
+                            result = n1 / n2;
+                        }
+                        break;
+            
+            default: result = 0;
+                        
+        }
+        setRta(result);
+    }
+    return (
+        <div style={{
+            padding: '20px',
+            maxWidth: '300px',
+            margin: '0 auto',
+            fontFamily: 'sans-serif'
+        }}>
+        <h2>Calculadora con react</h2>
+        <form onSubmit={calcular}
+        style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '10px'
+            }}>
+            {/*Solicitar el primer numero*/}
+            <input type="number" 
+                placeholder="Digite el primer numero"
+                value={num1}
+                onChange={(e)=>setNum1(e.target.value)}
+                />    
+            <select value={signo}
+                    onChange={(e)=>setSigno(e.target.value)}
+            >
+                <option value="suma">+</option>
+                <option value="resta">-</option>
+                <option value="multi">*</option>
+                <option value="divi">/</option>
+            </select>
+            {/*Solicitar el segundo numero*/}
+            <input type="number" 
+                placeholder="Digite el segundo numero"
+                value={num2}
+                onChange={(e)=>setNum2(e.target.value)}
+                />    
+            {/*Boton de accion*/}
+            <button type="submit">Calcular</button>
+            </form>
+            {/* Mostrar el resultado */}
+            {rta !== null && (<div style={{
+                                        marginTop:'13px',
+                                        fontWeight: 'bold',
+                                            }}>Resultado: {rta}</div>)}
+        </div>
+    )
+}
--- react-unilibre/src/main.jsx
+++ react-unilibre/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 //NO se le coloco {} a usuario porque tenia en el jsx el default
-import {Sumar} from './components/hooks/Sumar.jsx';
+import {Calculadora} from './components/Calculadora/Calculadora.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Sumar />
+    <Calculadora />
   </StrictMode>,
 )
